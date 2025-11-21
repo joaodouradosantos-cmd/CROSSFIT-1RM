c84e74c63012f3efa02ae4af592a0a08dae206d0
joaodouradosantos-cmd <joaodouradosantos@gmail.com>
2025-11-21 18:08:44 +0000

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,27 @@
-const CACHE_NAME = "crossfit-1rm-cache-v1";
-const ASSETS = [
+const CACHE = "crossfit-1rm-cache-v1";
+const FILES = [
   "./",
   "./index.html",
   "./manifest.json"
 ];
 
-self.addEventListener("install", (event) => {
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+    caches.open(CACHE).then(cache => cache.addAll(FILES))
   );
 });
 
-self.addEventListener("fetch", (event) => {
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(k => k !== CACHE).map(k => caches.delete(k))
+      )
+    )
+  );
+});
+
+self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(resp => resp || fetch(event.request))
   );
